43aee903ed7b08273c4bdc0bdc243e13c628b13b
Emma Wedekind <emma.wedekind@logmein.com>
2020-05-05 11:02:06 +0200

    Routing checklist pages

---
+++ src/pages/checklist.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const ChecklistPage = () => (
+  <Layout>
+    <h1>Checklist</h1>
+  </Layout>
+)
+
+export default ChecklistPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,18 @@
-import React, { useState } from "react"
-import Intro from "../components/intro"
+import React from "react"
+import { Link } from "gatsby"
 import Layout from "../components/layout"
 import ChecklistContainer from "../components/checklist-container"
 
-const IndexPage = () => {
-  const [checklistStarted, setChecklistStarted] = useState(false)
-
-  if (!checklistStarted) {
-    return (
-      <Layout>
-        <Intro setChecklistStarted={setChecklistStarted} />
-      </Layout>
-    )
-  } else {
-    return (
-      <Layout>
-        <ChecklistContainer />
-      </Layout>
-    )
-  }
-}
+const IndexPage = () => (
+  <Layout>
+    <p>
+      Welcome to the inclusive design checklist. This initiative was started as
+      a method for educating designers and developers about accessibility and
+      removing the stigma that it's difficult to learn. To get started, click
+      the start button below.
+    </p>
+    <Link to="/checklist">Start</Link>
+  </Layout>
+)
 
 export default IndexPage
