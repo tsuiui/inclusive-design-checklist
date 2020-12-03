d6e3b24841a08cf48b6f8eeeeb7c21eec7b9568a
Carl Tsui <carl.tsui@logmein.com>
2020-12-03 13:45:46 -0500

    Modify results list to include acceptance criteria, roles, and copy to clipboard button for each guideline

--- src/components/checklist-item.js
+++ src/components/checklist-item.js
@@ -2,7 +2,24 @@ import React from "react"
 import newPage from "../images/new-page.svg"
 
 export const ChecklistItem = ({ guideline, guidelineParams }) => {
-  const { link, title, goal, solution, impact } = guidelineParams
+  const { 
+    link, title, goal, solution, impact, design, writing, engineering, 
+    acceptance 
+  } = guidelineParams
+
+  let acRef = React.createRef();
+
+  const copyToClipboard = async (criteriaRef) => {
+    // Clipboard API not available
+    if (!navigator.clipboard) return
+    const text = criteriaRef.current.innerText;
+    try {
+      await navigator.clipboard.writeText(text)
+    } catch (err) {
+      console.error("Failed to copy!", err)
+    }
+  }
+
   return (
     <div className="checklist-item-wrapper" key={link}>
       <div className="checklist-title-wrapper">
@@ -14,8 +31,26 @@ export const ChecklistItem = ({ guideline, guidelineParams }) => {
       </div>
       <h2 className="checklist-goal">Goal</h2>
       <p>{goal}</p>
+      
+      <h2>Acceptance Criteria</h2>
+      {acceptance ? 
+        <>
+          <p ref={acRef}>{acceptance}</p>
+          <button type="button" className="checklist-item__copy-button" onClick={()=>copyToClipboard(acRef)}>
+            Copy Criteria
+          </button>
+        </>
+        :
+        <p>Not available</p>
+      }
       <h2>Solution</h2>
       <p>{solution}</p>
+      <h2>Roles Involved</h2>
+      <div className="checklist-item__roles">
+      {design && <span className="checklist-item__roles__job">Design</span>}
+      {writing && <span className="checklist-item__roles__job">Writing</span>}
+      {engineering && <span className="checklist-item__roles__job">Engineering</span>}
+      </div>
       <a
         href={link}
         className="guideline-badge"
